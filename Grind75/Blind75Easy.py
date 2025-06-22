5e0c76b310f83489d1178e3ce97ea5b4d886e357
Safiya Yunaeva <syunaeva@ulduzsoft.com>
2025-06-21 21:18:21 -0700

    Blind75Easy: merge strings

--- Grind75/Blind75Easy.py
+++ Grind75/Blind75Easy.py
@@ -1,5 +1,26 @@
 from typing import List
 
+def mergeAlternately(word1: str, word2: str) -> str:
+    """
+    You are given two strings word1 and word2. Merge the strings by adding letters in alternating order,
+    starting with word1. If a string is longer than the other, append the additional letters onto the end
+    of the merged string.
+    Input: word1 = "abc", word2 = "pqr"
+    Output: "apbqcr"
+    Input: word1 = "ab", word2 = "pqrs"
+    Output: "apbqrs"
+
+    Input: word1 = "abcd", word2 = "pq"
+    Output: "apbqcd"
+    """
+    result = ""
+
+    return result
+
+def test_mergeAlternately():
+    pass
+
+
 def runningSum(nums: List[int]) -> List[int]:
     """
     https://leetcode.com/problems/running-sum-of-1d-array/
@@ -11,7 +32,6 @@ def runningSum(nums: List[int]) -> List[int]:
         result.append(result[i - 1] + nums[i])
 
     return result
-
 
 def is_balanced(string_to_check):
     open = "("
@@ -26,6 +46,27 @@ def is_balanced(string_to_check):
         return True
     else:
         return False
+
+def compress(chars: List[str]) -> int:
+    # two pointers
+
+    i = 0
+    res = 0
+
+    while i < len(chars):
+        group_len = 1
+        while i < len(chars) - 1 and chars[i] == chars[i + 1]:
+            group_len += 1
+            i += 1
+        chars[res] = chars[i]
+        res += 1
+        if group_len > 1:
+            for val in str(group_len):
+                chars[res] = val
+                res += 1
+        i += 1
+
+    return res
 
 
 
@@ -62,34 +103,10 @@ def twosum(nums: List[int], target: int) -> List[int]:
         cache[target - value] = index
     return [0, 0]
 
+
 def test_twosum():
     a = "((a+b)*c)+(b*a)"
     assert is_balanced(a) is True
  #   print(is_balanced(a))
  #   print(matching_parenteces(a))
-
-from typing import List
-
-
-def compress(chars: List[str]) -> int:
-    # two pointers
-
-    i = 0
-    res = 0
-
-    while i < len(chars):
-        group_len = 1
-        while i < len(chars) - 1 and chars[i] == chars[i + 1]:
-            group_len += 1
-            i += 1
-        chars[res] = chars[i]
-        res += 1
-        if group_len > 1:
-            for val in str(group_len):
-                chars[res] = val
-                res += 1
-        i += 1
-
-    return res
-
 
