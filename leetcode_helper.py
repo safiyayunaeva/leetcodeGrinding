848b46d46d47e6c832e40f89e3c53ced4fa22603
Safiya Yunaeva <syunaeva@ulduzsoft.com>
2025-07-11 14:57:08 -0700

    Updated leetcode helper

--- leetcode_helper.py
+++ leetcode_helper.py
@@ -1,16 +1,13 @@
 import re
-from fileinput import filename
-from typing import List
-
 from bs4 import BeautifulSoup
 import textwrap
-import json
 import subprocess
+import requests
 
 
 def github_commit(file_name, s):
     filename = file_name  # Replace with your actual file
-    commit_message = f"Add {s}"
+    commit_message = f"Created  {s}"
     branch = "main"  # Change to your branch name if needed
 
     try:
@@ -71,16 +68,46 @@ def get_examples(s) ->  list[dict[str, str]]:
 
     return  examples
 
+def get_question_info(title_slug):
+        url = "https://leetcode.com/graphql"
+        session = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X3ZlcmlmaWVkX2VtYWlsIjpudWxsLCJhY2NvdW50X3VzZXIiOiJheDF0ZiIsIl9hdXRoX3VzZXJfaWQiOiIxODMzODE2MyIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjVjYTBkZmMzZDcyOTExYzFlN2U5MjM3YzY3M2EyMmI3YjQyZGQ5YzU2NTY0YmNhODE5NzVkYzdhY2M0NWNjNWQiLCJzZXNzaW9uX3V1aWQiOiJmM2M0OTgyNyIsImlkIjoxODMzODE2MywiZW1haWwiOiJ5dWxkdXouY2FsaWZvcm5pYUBnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImVsaXNhZ2F0ZXMiLCJ1c2VyX3NsdWciOiJlbGlzYWdhdGVzIiwiYXZhdGFyIjoiaHR0cHM6Ly9hc3NldHMubGVldGNvZGUuY29tL3VzZXJzL2RlZmF1bHRfYXZhdGFyLmpwZyIsInJlZnJlc2hlZF9hdCI6MTc1MjI1NzkxMywiaXAiOiI1MC4xNjguNTQuMjIiLCJpZGVudGl0eSI6ImNlNjliODUxYzRlZGM3ZWViZmIzOTk4YWE5NGE3MTU3IiwiZGV2aWNlX3dpdGhfaXAiOlsiNTAxZGMwOTcxZGUxYTIwNjUyZjkzMzgzZmY3YmMxNjIiLCI1MC4xNjguNTQuMjIiXX0.MOQ75TDj1DqR5M1ku5idCBcBtOqidkz5HMjvv874FBg"
 
-# Read JSON from file
-with open("payload.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
+        token = "DJn0HRdmqjvZMk8WsI3ilc3yum73VXOB5dgo8wS8RNt8zYZeAzUYbQ9QYQ4j9M7a"
+        headers = {
+            "Content-Type": "application/json",
+            "Referer": f"https://leetcode.com/problems/{title_slug}/",
+            f"Cookie": f"LEETCODE_SESSION={session}; csrftoken={token}",
+            f"x-csrftoken": f"{token}",
+            "User-Agent": "Mozilla/5.0"
+        }
 
+        payload = {
+            "operationName": "questionData",
+            "variables": {"titleSlug": title_slug},
+            "query": """
+            query questionData($titleSlug: String!) {
+              question(titleSlug: $titleSlug) {
+                questionId
+                title
+                difficulty
+                content
+                codeSnippets {
+                  langSlug
+                  code
+                }
+              }
+            }
+            """
+        }
+        response = requests.post(url, json=payload, headers=headers)
+
+        return response.json()
+
+
+slug = "remove-duplicates-from-sorted-array"
+data = get_question_info(slug)
 payload = data["data"]["question"]
 python_snippet = next( (item["code"] for item in payload["codeSnippets"] if item["langSlug"] == "python3"), None )
-
-print(payload["title"])
-
 soup = BeautifulSoup(payload["content"], "html.parser")
 cleaned = soup.get_text()
 
@@ -89,21 +116,13 @@ examples = get_examples(cleaned)
 
 code_lines = python_snippet.splitlines()
 new_snippet = []
-func_name = "example"
+func_name = re.sub(r'-', '_', slug)
 if "List[" in python_snippet:
     new_snippet.append("from typing import List\n")
 for line in code_lines:
     if not "class Solution" in line:
         if "def" in line:
             line = textwrap.dedent(line)
-            match = re.search(r'def\s+(\w+)\s*\(', line)
-
-            if match:
-                func_name = match.group(1)
-                print("Function name:", func_name)
-            else:
-                print("No function name found.")
-
         if "self" in line:
             line = re.sub(r'\bself,\s*', '', line)
             new_snippet.append(line)
@@ -121,7 +140,7 @@ new_snippet.append("\n")
 
 # add tests
 for i, ex in enumerate(examples, 1):
-    new_snippet.append(f"def test_example_{i}():")
+    new_snippet.append(f"def test_{func_name}_example_{i}():")
     new_snippet.append("\t\"\"\"")
     new_snippet.append("\t" + f"Example {i}:")
     new_snippet.append("\t" + "Input:" + ex["input"])
@@ -134,9 +153,9 @@ for i, ex in enumerate(examples, 1):
 
 code = "\n".join(new_snippet)
 
-output_file = f"Templates/{func_name}_template.py"
+output_file = f"TopInterview150/{func_name}_template.py"
 
 with open(output_file, "w") as f:
     f.write(code)
 
-github_commit(output_file, payload["questionId"] + " " + payload["title"])
+github_commit(output_file, f"{payload["questionId"]}:  {payload["title"]}")
