30b7c2f1c3ac411310bad2dd838a42f82d0b848a
Safiya Yunaeva <syunaeva@ulduzsoft.com>
2025-07-16 22:35:49 -0700

    Created  169:  Majority Element

---
+++ TopInterview150/majority_element_template.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def majorityElement(nums: List[int]) -> int:
+	"""
+	Majority Element
+	Given an array nums of size n, return the majority element.
+	The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+	
+	Constraints:
+	
+	n == nums.length
+	1 <= n <= 5 * 104
+	-109 <= nums[i] <= 109
+	
+	
+	"""
+	pass
+
