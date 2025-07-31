3fc6914140053ff38005a05083e524f8e4c071cd
Safiya Yunaeva <syunaeva@ulduzsoft.com>
2025-07-30 18:34:00 -0700

    Created  14:  Longest Common Prefix

---
+++ TopInterview150/longest_common_prefix_template.py
@@ -0,0 +1,38 @@
+from typing import List
+
+def longestCommonPrefix(strs: List[str]) -> str:
+	"""
+	Longest Common Prefix
+	Write a function to find the longest common prefix string amongst an array of strings.
+	If there is no common prefix, return an empty string "".
+	
+	Constraints:
+	
+	1 <= strs.length <= 200
+	0 <= strs[i].length <= 200
+	strs[i] consists of only lowercase English letters if it is non-empty.
+	
+	
+	"""
+	pass
+
+
+def test_longest_common_prefix_example_1():
+	"""
+	Example 1:
+	Input:strs = ["flower","flow","flight"]
+	Output:"fl"
+	Explanation:
+	"""
+	pass
+
+
+def test_longest_common_prefix_example_2():
+	"""
+	Example 2:
+	Input:strs = ["dog","racecar","car"]
+	Output:""
+	Explanation:There is no common prefix among the input strings.
+	"""
+	pass
+
