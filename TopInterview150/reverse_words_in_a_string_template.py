aa4f5df23cdf5da109251c98e21fbe5cb2abcb27
Safiya Yunaeva <syunaeva@ulduzsoft.com>
2025-07-30 18:34:16 -0700

    Created  151:  Reverse Words in a String

---
+++ TopInterview150/reverse_words_in_a_string_template.py
@@ -0,0 +1,48 @@
+def reverseWords(s: str) -> str:
+	"""
+	Reverse Words in a String
+	Given an input string s, reverse the order of the words.
+	A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+	Return a string of the words in reverse order concatenated by a single space.
+	Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+	
+	Constraints:
+	
+	1 <= s.length <= 104
+	s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+	There is at least one word in s.
+	
+	
+	"""
+	pass
+
+
+def test_reverse_words_in_a_string_example_1():
+	"""
+	Example 1:
+	Input:s = "the sky is blue"
+	Output:"blue is sky the"
+	Explanation:
+	"""
+	pass
+
+
+def test_reverse_words_in_a_string_example_2():
+	"""
+	Example 2:
+	Input:s = "  hello world  "
+	Output:"world hello"
+	Explanation:Your reversed string should not contain leading or trailing spaces.
+	"""
+	pass
+
+
+def test_reverse_words_in_a_string_example_3():
+	"""
+	Example 3:
+	Input:s = "a good   example"
+	Output:"example good a"
+	Explanation:You need to reduce multiple spaces between two words to a single space in the reversed string.
+	"""
+	pass
+
