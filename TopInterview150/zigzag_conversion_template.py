cd0c337b0a5c6cefacf3cc1d25028c2ecca3c68e
Safiya Yunaeva <syunaeva@ulduzsoft.com>
2025-07-30 18:34:34 -0700

    Created  6:  Zigzag Conversion

---
+++ TopInterview150/zigzag_conversion_template.py
@@ -0,0 +1,64 @@
+def convert(s: str, numRows: int) -> str:
+	"""
+	Zigzag Conversion
+	The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+	
+	P   A   H   N
+	A P L S I I G
+	Y   I   R
+	
+	And then read line by line: "PAHNAPLSIIGYIR"
+	Write the code that will take a string and make this conversion given a number of rows:
+	
+	string convert(string s, int numRows);
+	
+	Constraints:
+	
+	1 <= s.length <= 1000
+	s consists of English letters (lower-case and upper-case), ',' and '.'.
+	1 <= numRows <= 1000
+	
+	
+	"""
+	pass
+
+
+def test_zigzag_conversion_example_1():
+	"""
+	Example 1:
+	Input:s = "PAYPALISHIRING", numRows = 3
+	Output:"PAHNAPLSIIGYIR"
+	Explanation:
+	"""
+	pass
+
+
+def test_zigzag_conversion_example_2():
+	"""
+	Example 2:
+	Input:s = "PAYPALISHIRING", numRows = 4
+	Output:"PINALSIGYAHRPI"
+	Explanation:P     I    N
+A   L S  I G
+Y A   H R
+P     I
+	"""
+	pass
+
+
+def test_zigzag_conversion_example_3():
+	"""
+	Example 3:
+	Input:s = "A", numRows = 1
+	Output:"A"
+
+ 
+Constraints:
+
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+	Explanation:
+	"""
+	pass
+
