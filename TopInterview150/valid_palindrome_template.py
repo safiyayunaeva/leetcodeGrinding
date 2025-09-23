4d2e63b09469ef297cde71d68b2629706ff77ee8
Safiya Yunaeva <syunaeva@ulduzsoft.com>
2025-09-22 23:09:57 -0700

    Created  125:  Valid Palindrome

---
+++ TopInterview150/valid_palindrome_template.py
@@ -0,0 +1,46 @@
+def isPalindrome(s: str) -> bool:
+	"""
+	Valid Palindrome
+	A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+	Given a string s, return true if it is a palindrome, or false otherwise.
+	
+	Constraints:
+	
+	1 <= s.length <= 2 * 105
+	s consists only of printable ASCII characters.
+	
+	
+	"""
+	pass
+
+
+def test_valid_palindrome_example_1():
+	"""
+	Example 1:
+	Input:s = "A man, a plan, a canal: Panama"
+	Output:true
+	Explanation:"amanaplanacanalpanama" is a palindrome.
+	"""
+	pass
+
+
+def test_valid_palindrome_example_2():
+	"""
+	Example 2:
+	Input:s = "race a car"
+	Output:false
+	Explanation:"raceacar" is not a palindrome.
+	"""
+	pass
+
+
+def test_valid_palindrome_example_3():
+	"""
+	Example 3:
+	Input:s = " "
+	Output:true
+	Explanation:s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+	"""
+	pass
+
